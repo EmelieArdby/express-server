ba75c207b4b4be9069252344fb2ab8435c746707
Emelie Ardby <emelie.ardby@schibsted.se>
2017-09-27 16:15:08 +0200

    hej hopp

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require ('express')
 const app = express ()
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get ('/banana', function (req,res){
+	console.log('banana')
+	res.render('fruit', {fruit: 'banana'})
+
+})
+
+app.get ('/apelsin', function (req,res){
+	console.log('apelsin')
+	res.render('fruit', {fruit: 'apelsin'})
+
+})
+
 
 app.listen(3000, function (){
 	console.log('App acting as port 3000! Visit localhost:3000 in Chrome')
-})
-app.get('/', function (req,res) {
- 	console.log('Nu är vi här'),
-  	res.send('hello cats')
 })
 
 app.get ('/om', function (req,res){
@@ -18,3 +33,4 @@ app.get ('/kontakt', function (req,res){
 	console.log('Nu är vi här')
 	res.send('Här når du mig: emelie.ardby@schisbted.com')
 })
+
